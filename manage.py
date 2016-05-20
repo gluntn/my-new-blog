489e672ae4f657eb3bec710dcb21b3820595b9e1
William <wostensen96@gmail.com>
2016-05-20 23:06:23 +0200

    Fixing manage.py

--- manage.py
+++ manage.py
@@ -3,12 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newblog.settings")
-=======
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
->>>>>>> 00d492edd3d17150c0b3ab2ca47205f7d62278c3
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newblog.settings")
+	
+	from django.core.management import execute_from_command_line
+	execute_from_command_line(sys.argv)
